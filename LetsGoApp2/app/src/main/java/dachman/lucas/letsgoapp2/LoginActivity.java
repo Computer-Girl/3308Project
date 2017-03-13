2ff978c7bda7ed5071d538af13ad16bc4b831d5c
LucasDachman <lucas.dachman@gmail.com>
2017-03-12 19:22:39 -0600

    Added more atributes to the EventViewActivity and added a maps button

--- LetsGoApp2/app/src/main/java/dachman/lucas/letsgoapp2/LoginActivity.java
+++ LetsGoApp2/app/src/main/java/dachman/lucas/letsgoapp2/LoginActivity.java
@@ -18,6 +18,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //TODO: Add real login activity
 
     }
 
