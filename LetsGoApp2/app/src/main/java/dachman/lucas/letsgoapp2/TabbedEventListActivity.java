e9d668c265cf2d69a42a0429b852a05305df95b6
Jonathan Taing <taingj@colorado.edu>
2017-03-27 21:18:53 -0600

    Removed native ActionBar and replaced it with the Toolbar. Also, added an overflow menu with user sign out.

--- LetsGoApp2/app/src/main/java/dachman/lucas/letsgoapp2/TabbedEventListActivity.java
+++ LetsGoApp2/app/src/main/java/dachman/lucas/letsgoapp2/TabbedEventListActivity.java
@@ -1,14 +1,27 @@
 package dachman.lucas.letsgoapp2;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 import dachman.lucas.letsgoapp2.Adapters.mFragmentPagerAdapter;
 import dachman.lucas.letsgoapp2.Models.Category;
 
 public class TabbedEventListActivity extends AppCompatActivity {
+
+    private GoogleApiClient mGoogleApiClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +36,59 @@ public class TabbedEventListActivity extends AppCompatActivity {
         // Give Tab Layout the view pager
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(viewPager);
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
     }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu , menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onStart() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            // Add logout method here.
+            signOut();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // [START signOut]
+    private void signOut() {
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        // [START_EXCLUDE]
+                        //updateUI(false);
+                        // [END_EXCLUDE]
+                        Intent logoutIntent = new Intent(TabbedEventListActivity.this, LoginActivity.class);
+                        startActivity(logoutIntent);
+                        finish();
+                    }
+                });
+    }
+    // [END signOut]
 }
